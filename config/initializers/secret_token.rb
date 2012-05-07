1b944f9d59654400886bed655fbb9d5cd566b724
Federico Brubacher <fbrubacher@gmail.com>
2012-05-06 23:28:11 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kross::Application.config.secret_token = '09084cfe3b6df28b55471f479f1bd9a9e5fab686e5645214ed53b0328399384b5b4cc6e826fb5ed0616e8bc590f7ecd30c0e2eb46f7b1687c41819db1ac6ae22'
